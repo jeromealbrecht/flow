ae6ffc2d73898f808472ed2294e094b32725e4b7
December81 <megastudio@outlook.fr>
2025-03-29 17:47:46 +0100

    Suppression des fichiers de mise en page et de gestion des utilisateurs dans le composant Studio, ainsi que des fichiers associés à l'administration.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,23 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["images.unsplash.com"], // Ajoute le domaine Unsplash
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Applique à toutes les routes
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups", // Permet l'ouverture des popups
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "credentialless", // Option pour éviter les blocages
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
